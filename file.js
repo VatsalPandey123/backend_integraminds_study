bc301ed97b671731f4f3572146058f8f1f86889a
VatsalPandey123 <vatsalnarayan123@gmail.com>
2026-02-12 11:05:09 +0530

    done at 12-02-2026

--- file.js
+++ file.js
@@ -1,4 +1,24 @@
-// import add , multiply , div from './utils.js';
-import add from './utils.js';
-const output = add(2,3);
-console.log("output:", output);
+// // import add , multiply , div from './utils.js';
+// import add from './utils.js';
+// const output = add(2,3);
+// console.log("output:", output);
+
+const fetchData = new Promise((resolve, reject) =>{
+    const success = false;
+
+    if(success){
+        resolve("Data fetched Successfully");
+        console.log("data fetched")
+    }else {
+        reject("No data found");
+        console.log("data is not fetched");
+    }
+});
+
+fetchData
+    .then((message)=>{
+        console.log(message);
+    })
+    .catch((err)=>{
+    console.log(err);
+})
