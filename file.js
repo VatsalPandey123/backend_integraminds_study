4abfb4793b92b38734b0d9e7791e0b6db55707fe
VatsalPandey123 <vatsalnarayan123@gmail.com>
2026-02-16 16:41:03 +0530

    16-02-2026 done

--- file.js
+++ file.js
@@ -3,22 +3,34 @@
 // const output = add(2,3);
 // console.log("output:", output);
 
-const fetchData = new Promise((resolve, reject) =>{
-    const success = false;
+// const fetchData = new Promise((resolve, reject) =>{
+//     const success = false;
 
-    if(success){
-        resolve("Data fetched Successfully");
-        console.log("data fetched")
-    }else {
-        reject("No data found");
-        console.log("data is not fetched");
-    }
-});
+//     if(success){
+//         resolve("Data fetched Successfully");
+//         console.log("data fetched")
+//     }else {
+//         reject("No data found");
+//         console.log("data is not fetched");
+//     }
+// });
 
-fetchData
-    .then((message)=>{
-        console.log(message);
-    })
-    .catch((err)=>{
-    console.log(err);
-})
+// fetchData
+//     .then((message)=>{
+//         console.log(message);
+//     })
+//     .catch((err)=>{
+//     console.log(err);
+// })
+const sample = async ()=>{
+    console.log("Started");
+
+    await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(response => response.json())
+    .then((json) => console.log(json))
+
+    console.log("Finished");
+};
+console.log("sample ~ started 1");
+ sample();
+ console.log("sample ~ started 2");
